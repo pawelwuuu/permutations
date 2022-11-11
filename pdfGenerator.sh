36132ad42cfca4368120c3a660bf213c3eb8cf67
Pawel Wojcik <wojcikp376@gmail.com>
2022-11-11 15:39:00 -0500

    added validation for bash script

--- pdfGenerator.sh
+++ pdfGenerator.sh
@@ -1,33 +1,74 @@
 #!/bin/bash
 
-filename=$1
 
-#putting parameters form config file into the array.
-counter=0
+filename=$1
+#checking if configuration file exists
+if ! [ -e $filename ]
+then
+    echo "Unable to find or open configuration file."
+    exit
+fi
+
+
+firstIterationFlag="true"
 
 chapters=0
 setLength=()
 permutationsAmount=()
 
-#loading parameters to arrays: setLengh and permutationsAmount.
+#loading parameters to arrays: setLength and permutationsAmount.
 while read line; do
-    if (($counter==0)); then
-        chapters=$line
-        counter=1
-        
-        continue
+    if [ $firstIterationFlag = 'true' ]
+        then
+            chapters=$line
+            firstIterationFlag='false'
+
+            continue
     fi
 
     #parameters is an array that stores splited parameters from read
     #line e.g line (2 5) and line (8 6) will be stored in
     #parameters array like that [2, 5, 8, 6].
     parameters=()
-    for value in $line
-    do
+    for value in $line; do
         parameters+=($value)
     done
 
-    setLength[$counter-1]=${parameters[0]}
-    permutationsAmount[$counter]=${parameters[1]}
-    counter+=1
+    setLength+=(${parameters[0]})
+    permutationsAmount+=(${parameters[1]})
 done < $filename
+
+#Validating parameters
+#checking if file ends with empty line character
+fileContent=`cat -e $filename`
+if [[ ${fileContent: -1} != '$' ]]; then
+    echo "Invalid configuration parameters, there should be an empty line at the end of file."
+    exit
+fi
+
+#checking if parameters are digits
+for parameter in ${setLength[@]}
+do
+    if ! [[ $parameter =~ [0-9]+ ]] ; then
+        echo "Invalid configuration parameters, there should be only positive numbers in config file."
+        exit
+    fi
+done
+
+for parameter in ${permutationsAmount[@]}
+do
+    if ! [[ $parameter =~ [0-9]+ ]] ; then
+        echo echo "Invalid configuration parameters, there should be only positive numbers in config file."
+        exit
+    fi
+done
+
+#checking if set lenght parameters are in limit of 100
+for parameter in ${setLength[@]}
+do
+    if [[ $parameter -gt 100 ]] ; then
+        echo "Invalid configuration parameters, limit of set length is 100."
+        exit
+    fi
+done
+
