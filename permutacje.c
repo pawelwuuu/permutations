a924c9241bc54cac75c117b9f72ce83889157f9b
SzefuKox123PL <szyrrogi@hotmail.com>
2022-11-20 12:14:17 +0100

    zmienilem permutacje.c

--- permutacje.c
+++ permutacje.c
@@ -1,100 +1,203 @@
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
 #include <time.h>
 #include <stdlib.h>
 
-int g=0;      //pomocnicze zmienne
-int pomoc=0;
-int wypisane=0;
-int ktory=0;
+int g = 0;	//pomocnicze zmienne
+int pomoc = 0;
+int wypisane = 0;
+int ktory = 0;
+int bylo[62];
 
-void wypisywanie(int wartosci[],int dlugosc,char *str, int ile)
+char tworzenie()
 {
-  int wypisz=1;
-  int pomoc2[dlugosc+1];
-    for(int j=0;j<dlugosc;j++)      //uzupelnienie inta zerami
-    {
-     pomoc2[j]=0;
-    }
-  for(int j=0;j<dlugosc;j++)    //sprawdzanie czy jest to replikacja a nie kombinacja z powtorzeniami typu abc -> cbb
-    {
-     pomoc2[wartosci[j]]++;
-    }
-    for(int j=1;j<dlugosc;j++)
-    {
-     if(pomoc2[j]!=1){wypisz=0;};
-    }
-  int potenga=1;
-if(wypisz)      //dzialania tylko na poprawnych replikacjach
-{
-  ktory++;      //liczy ilosc replikacji
-  for(int i=1; i<=dlugosc; i++)
-    {
-      potenga=potenga*i;
-    }
-    
-  if(potenga-wypisane>potenga-ile)      //pseudolosuje ktore replikacje wypisac a ktore nie
-  {
-  if((potenga-ktory)+1!=ile-wypisane)
-  {
-    int r = rand()%potenga; 
-    r++;
-    if(r>ile)
-    {
-      wypisz=0;
-    }
-  }
-  else
-  {
-    wypisz=1;
-  }
-  }
-  else{wypisz=0;}
+	int wyjdz = 1;
+	int r;
+	do {
+		r = rand() % 60;
+		if (bylo[r] == 0)
+		{
+			wyjdz = 0;
+			bylo[r] = 1;
+			if (r < 10)
+			{
+				r = r + 48;
+			}
+			else
+			{
+				if (r < 36)
+				{
+					r = r + 55;
+				}
+				else
+				{
+					r = r + 61;
+				}
+			}
+		}
+	}
+
+	while (wyjdz == 1);
+	return r;
+
 }
 
-if(wypisz)      
+int wypisywanie(int wartosci[], int dlugosc, char *str, char *str2, int ile)
 {
-  for(int j=0;j<dlugosc;j++)
-    {
-    printf("%c",str[wartosci[j]-1]);
-    }
-  printf("\n");
-  wypisane++;
-  }
+	if (ile == wypisane)
+	{
+		return 0;
+	}	//optymalizacja
+	int wypisz = 1;
+	int pomoc2[dlugosc + 1];
+	for (int j = 0; j < dlugosc; j++)	//uzupelnienie inta zerami
+	{
+		pomoc2[j] = 0;
+	}
+
+	for (int j = 0; j < dlugosc; j++)	//sprawdzanie czy jest to replikacja a nie kombinacja z powtorzeniami typu abc -> cbb
+	{
+		pomoc2[wartosci[j]]++;
+	}
+
+	for (int j = 1; j < dlugosc; j++)
+	{
+		if (pomoc2[j] != 1)
+		{
+			wypisz = 0;
+		};
+
+	}
+
+	int potenga = 1;
+	if (wypisz)	//dzialania tylko na poprawnych replikacjach
+	{
+		ktory++;	//liczy ilosc replikacji
+		if (dlugosc > 7)
+		{
+			potenga = 5040;
+		}
+		else
+		{
+			for (int i = 1; i <= dlugosc; i++)
+			{
+				potenga = potenga * i;
+			}
+		}
+
+		if (potenga - wypisane > potenga - ile)	//pseudolosuje ktore replikacje wypisac a ktore nie
+		{
+			if ((potenga - ktory) + 1 != ile - wypisane)
+			{
+				int r = rand() % potenga;
+				r++;
+				if (r > ile)
+				{
+					wypisz = 0;
+				}
+			}
+			else
+			{
+				wypisz = 1;
+			}
+		}
+		else
+		{
+			wypisz = 0;
+		}
+	}
+
+	if (wypisz)	//wypisuje
+	{
+		for (int j = 0; j < dlugosc; j++)
+		{
+			printf("%c", str[wartosci[j] - 1]);
+		}
+
+		printf("%s\n", str2);
+		wypisane++;
+	}
 }
 
-void replikacja(int dlugosc,char *str,int wartosci[],int ile)  //tworzy KOMBINACJE z powtórzeniami za pomocą replikacji
+int replikacja(int dlugosc, char *str, char *str2, int wartosci[], int ile)	//tworzy KOMBINACJE z powtórzeniami za pomocą replikacji
 {
-  for(int i=1; i<dlugosc+1;i++)
-    {
-      if(g==dlugosc)
-      {
-        i++;
-        if(wartosci[dlugosc-1]!=pomoc)
-          {
-        wypisywanie(wartosci,dlugosc,str, ile);       
-          }
-          pomoc=wartosci[dlugosc-1];
-      }
-      else
-      {
-        wartosci[g]=i;
-        g++;
-        replikacja(dlugosc,str,wartosci, ile);
-        
-      }
-    }
-  g=g-1;
+	for (int i = 1; i < dlugosc + 1; i++)
+	{
+		if (ile == wypisane)
+		{
+			return 0;
+		}	//optymalizacja
+		if (g == dlugosc)
+		{
+			i++;
+			if (wartosci[dlugosc - 1] != pomoc)
+			{
+				wypisywanie(wartosci, dlugosc, str, str2, ile);
+				if (ile == wypisane)
+				{
+					return 0;
+				}	//optymalizacja    
+			}
+
+			pomoc = wartosci[dlugosc - 1];
+		}
+		else
+		{
+			wartosci[g] = i;
+			g++;
+			replikacja(dlugosc, str, str2, wartosci, ile);
+			if (ile == wypisane)
+			{
+				return 0;
+			}	//optymalizacja
+
+		}
+	}
+
+	g = g - 1;
 }
 
-int main() 
-{ 
-   srand(time(NULL));
-    char str[] = "abcd";       //wartosc z ktorej bedziemy robic permutacje
-    int dlugosc = strlen(str);   
-    int ile=4;            //ilosc permutacji
-    int wartosci[dlugosc];
-    replikacja(dlugosc,str,wartosci,ile);
-    return 0; 
-} 
+int main(int argc, char *argv[])
+{
+	srand(time(NULL));
 
+	int du = atoi(argv[1]);
+	int dlugosc = du;
+	char str0[du];
+	for (int i = 0; i < dlugosc; i++)
+	{
+		char p = tworzenie();
+		str0[i] = p;
+	}
+
+	str0[dlugosc] = '\0';
+
+	printf("Permutacja glowna: %s\n", str0);
+	char str[10];
+	char str2[10];
+	strncpy(str, str0, 10);
+	strncpy(str2, str0 + 9, 10);
+	str[9] = '\0';
+	str2[9] = '\0';
+	int ile = 1;
+	if (*argv[2] == *"A")
+	{
+		for (int i = 1; i <= dlugosc; i++)
+		{
+			ile = ile * i;
+		}
+	}
+	else
+	{
+		ile = atoi(argv[2]);	//ilosc implikacji
+	}
+
+	int wartosci[dlugosc];
+	for (int i = 0; i < dlugosc; i++)
+	{
+		wartosci[i] = i;
+	}
+	if(dlugosc>10){dlugosc=9;}
+	replikacja(dlugosc, str, str2, wartosci, ile);
+	return 0;
+}
